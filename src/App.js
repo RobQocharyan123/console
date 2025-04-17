0e1a6d1802df5ee332fbbc827071f23cf67e0401
RobQocharyan <robqocharyan@mail.ru>
2025-04-17 23:11:46 +0400

    bugfix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,109 @@
-import { useEffect } from "react";
 import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { Suspense, lazy, useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { loginPostUserData } from "./Commons/Services/homePageService.js";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getHomePageDataThunk,
+  loginTelegramBotThunk
+} from "./Store/Middlewares/homePageData.js";
+
+// Lazy load components
+const Header = lazy(() => import("./Commons/Components/Header/Header"));
+const Home = lazy(() => import("./Commons/Components/Home/Home"));
+const NavBar = lazy(() => import("./Commons/Components/NavBar/NavBar"));
+const Boost = lazy(() => import("./Commons/Components/Boost/Boost"));
+const LogoAnimation = lazy(() =>
+  import("./Commons/Components/LogoAnimation/LogoAnimation")
+);
+const Tasks = lazy(() => import("./Commons/Components/Tasks/Tasks"));
+const AirDrop = lazy(() => import("./Commons/Components/AirDrop/AirDrop"));
+const Profile = lazy(() => import("./Commons/Components/Profile/Profile"));
+const Friends = lazy(() => import("./Commons/Components/Friends/Friends"));
 
 function App() {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const homeData = useSelector((state) => state?.homePage?.homeData);
+
+  const isSuccess = useSelector((state) => state?.telegramLogin?.isSuccess);
+  const token = useSelector((state) => state?.telegramLogin?.token);
+
   useEffect(() => {
     const tg = window.Telegram?.WebApp;
 
-    const user = tg?.initDataUnsafe?.user;
+    const userData = tg?.initDataUnsafe?.user;
 
-    if (user) {
-      console.log("✅ Telegram User Info:", user); // Will log the user info here
+    if (userData) {
+      dispatch(loginTelegramBotThunk(userData));
+      console.log("✅ Telegram User Info:", userData);
     } else {
       console.warn("⚠️ User not available. Open inside Telegram WebApp.");
     }
   }, []);
 
-  return <div className="app">Telegram Boto</div>;
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(getHomePageDataThunk({ token }));
+      navigate("/home");
+    }
+  }, [isSuccess, dispatch]);
+
+  useEffect(() => {
+    const tg = window.Telegram?.WebApp;
+
+    if (tg) {
+      tg.expand();
+
+      tg.onEvent("viewport_changed", (event) => {
+        const { width, height, is_expanded } = event.data;
+
+        if (!is_expanded) {
+          tg.expand();
+        }
+
+        console.log(
+          `Viewport changed: width=${width}, height=${height}, is_expanded=${is_expanded}`
+        );
+      });
+    }
+
+    if (!homeData) {
+      return <LogoAnimation />;
+    }
+    return () => {
+      if (tg) {
+        tg.offEvent("viewport_changed");
+      }
+    };
+  }, []);
+
+  return (
+    <div className="app">
+      <Suspense fallback={<LogoAnimation />}>
+        <Header />
+        <div className="main-scroll">
+          {/* 👈 Scrollable wrapper */}
+          <Routes>
+            <Route path="/home" element={<Home />}>
+              <Route path="boost" element={<Boost />} />
+            </Route>
+            <Route path="/tasks" element={<Tasks />}>
+              <Route path="boost" element={<Boost />} />
+            </Route>
+            <Route path="/airdrop" element={<AirDrop />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/friends" element={<Friends />} />
+          </Routes>
+        </div>
+        <NavBar />
+        <ToastContainer />
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
--- src/Commons/Services/homePageService.js
+++ src/Commons/Services/homePageService.js
@@ -1,46 +1,46 @@
-import axios from 'axios';
-import pureClient from '.';
+import axios from "axios";
+import pureClient from ".";
 
-// if (typeof window.Telegram === "undefined") {
-//   window.Telegram = {
-//     WebApp: {
-//       expand: () => {},
-//       initDataUnsafe: {
-//         user: {
-//           id: "1802368420",
-//           first_name: "",
-//           last_name: "Poghosyan",
-//           username: "Poghos_kkk",
-//           language_code: "en",
-//           photo_url:
-//             "https://t.me/i/userpic/320/R6kP81fplPdhuT-LUfFQPEUXqqKPrvaTLmqSgpUeMfc.jpg",
-//           hash: "a2ab797fbc1d209a618725edb826dc0736dee5d2a120bcdd7ae6b7d88415ab18"
-//         }
-//       }
-//     }
-//   };
-// }
+if (typeof window.Telegram === "undefined") {
+  window.Telegram = {
+    WebApp: {
+      expand: () => {},
+      initDataUnsafe: {
+        user: {
+          id: "1802368420",
+          first_name: "",
+          last_name: "Poghosyan",
+          username: "Poghos_kkk",
+          language_code: "en",
+          photo_url:
+            "https://t.me/i/userpic/320/R6kP81fplPdhuT-LUfFQPEUXqqKPrvaTLmqSgpUeMfc.jpg",
+          hash: "a2ab797fbc1d209a618725edb826dc0736dee5d2a120bcdd7ae6b7d88415ab18"
+        }
+      }
+    }
+  };
+}
 
 export const loginPostUserData = async (userData) => {
   if (!userData) {
-    console.log('User data is missing!');
+    console.log("User data is missing!");
     return;
   }
 
   try {
-    const response = await pureClient.post('auth/telegram', {
+    const response = await pureClient.post("auth/telegram", {
       id: userData.id,
       first_name: userData.first_name,
-      last_name: userData.last_name || '',
-      username: userData.username || '',
+      last_name: userData.last_name || "",
+      username: userData.username || "",
       language: userData.language_code,
-      photo_url: userData.photo_url || '',
-      hash: userData.hash || '',
+      photo_url: userData.photo_url || "",
+      hash: userData.hash || ""
     });
 
     return response;
   } catch (err) {
-    console.error('Error sending user info:', err);
+    console.error("Error sending user info:", err);
   }
 };
 
@@ -48,18 +48,18 @@ export const homePageGetData = async ({ token }) => {
   try {
     const response = await pureClient.get(`/user`, {
       headers: {
-        Authorization: `Bearer ${token}`,
-      },
+        Authorization: `Bearer ${token}`
+      }
     });
 
     return response.data;
   } catch (error) {
     console.error(
-      'Error fetching data from /user:',
+      "Error fetching data from /user:",
       error.response || error.message
     );
     if (error.response) {
-      console.error('Response data:', error.response.data);
+      console.error("Response data:", error.response.data);
     }
     throw error;
   }
@@ -71,12 +71,12 @@ export const homePageDailyCode = async (data, token) => {
       `/user/daily-code`,
       { code: data.code },
       {
-        headers: { Authorization: `Bearer ${token}` },
+        headers: { Authorization: `Bearer ${token}` }
       }
     );
     return response.data;
   } catch (error) {
-    console.error('Error fetching data from /user/daily-code:', error);
+    console.error("Error fetching data from /user/daily-code:", error);
     throw error;
   }
 };
@@ -87,12 +87,12 @@ export const homePageDailyPoint = async (token) => {
       `/user/daily-claim`,
       {},
       {
-        headers: { Authorization: `Bearer ${token}` },
+        headers: { Authorization: `Bearer ${token}` }
       }
     );
     return response;
   } catch (error) {
-    console.error('Error fetching data from /telegram:', error);
+    console.error("Error fetching data from /telegram:", error);
     throw error;
   }
 };
